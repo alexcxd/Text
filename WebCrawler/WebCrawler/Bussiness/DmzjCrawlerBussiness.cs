8a82be6632547fd64c4eedf27748b275b76d848d
alex_home <247225077@qq.com>
2018-12-04 00:47:38 +0800

    基本完成；现有问题：1.中断恢复的问题；2.部分章节未爬取到；3.可能存在线程饥饿问题

--- WebCrawler/WebCrawler/Bussiness/DmzjCrawlerBussiness.cs
+++ WebCrawler/WebCrawler/Bussiness/DmzjCrawlerBussiness.cs
@@ -8,6 +8,7 @@ using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
+using Microsoft.ClearScript.V8;
 using WebCrawler.Crawler;
 using WebCrawler.Model;
 using WebCrawler.Utils;
@@ -22,7 +23,7 @@ namespace WebCrawler.Bussiness
         private static readonly string firstReferer = "https://manhua.dmzj.com/";
         private static readonly string filePath = @"E:\Desktop\漫画";
         private static readonly string filePathLog = filePath + @"\Log.txt";
-        private static readonly string picture = "https://images.dmzj.com/h/";
+        private static readonly string pictureMain = "https://images.dmzj.com/";
         private static NameToUrlManage manager = new NameToUrlManage();
 
         public bool GetAllChapters()
@@ -68,6 +69,7 @@ namespace WebCrawler.Bussiness
                         var urls = Regex.Matches(links.Value,
                             @"<a[^>]+title=""*(?<title>[^>\s]+)""[^>]+href=""*(?<href>[^>\s]+)""\s*[^>]*>[^<]*</a>");
 
+                        var cartoonName = urls[0].Groups["title"].Value.Split('-')[0];
                         var urlQueue = new Queue<NameToUrl>();
                         foreach (Match url in urls)
                         {
@@ -91,7 +93,6 @@ namespace WebCrawler.Bussiness
 
             };
             crawler.Start(new Uri(uri), firstReferer).Wait();
-
             return true;
         }
 
@@ -128,41 +129,61 @@ namespace WebCrawler.Bussiness
             };
             crawler.OnCompleted += (s, e) =>
             {
-                var url = "";
+                var pictrueFilePath = "";
+                var pictrueUrlManaeger = new NameToUrlManage();
                 using (var stream = new FileStream(filePathLog, FileMode.Append, FileAccess.Write,
                     FileShare.Write))
                 {
                     using (var writer = new StreamWriter(stream))
                     {
-                        var links = Regex.Match(e.PageSource, @"<script type=""text/javascript"">(?!<script)*?</script>");
-                        var id = "";
-                        if (!String.IsNullOrEmpty(links.Value))
+                        var links = Regex.Match(e.PageSource, @"<script type=""text/javascript"">(?<script>[^$]*?)</script>");
+
+                        using (var engine = new V8ScriptEngine("debug-v8engine"))
                         {
-                            id = links.Value.Substring(3, links.Value.Length - 1);
+                            var script = links.Groups["script"].Value;
+                            script += "arr_pages = arr_pages.join(',')";
+                            engine.Execute(script);
+
+                            //获取图片url
+                            var values = (string)engine.Script.arr_pages;
+                            var pictrueUrls = values.Split(',');
+
+                            //获取章节名
+                            var comicName = (string)engine.Script.g_comic_name;
+                            var chapterName = (string)engine.Script.g_chapter_name;
+
+                            //创建相应文件夹
+                            pictrueFilePath = filePath + "\\" + comicName + "\\" + chapterName;
+                            if (!Directory.Exists(pictrueFilePath))
+                            {
+                                Directory.CreateDirectory(pictrueFilePath);
+                            }
+
+                            var queue = new Queue<NameToUrl>();
+                            foreach (var pictrueUrl in pictrueUrls)
+                            {
+                                queue.Enqueue(new NameToUrl
+                                {
+                                    Name = chapterName + "-" + chapterName,
+                                    Url = pictureMain + pictrueUrl
+                                });
+                            }
+                            pictrueUrlManaeger = new NameToUrlManage(queue);
+
+                            writer.WriteLine("爬虫抓取任务完成！开始步骤三！");
+                            writer.WriteLine("耗时：" + e.Milliseconds + "毫秒");
+                            writer.WriteLine("线程：" + e.ThreadId + "");
+                            writer.WriteLine("地址：" + e.Uri.ToString() + "");
+                            writer.WriteLine("结果：" + comicName + "-" + chapterName + "");
+                            writer.WriteLine("===============================================");
                         }
-                        var name = e.SpecialArguments["Name"].Split('-');
-                        url = picture + HttpUtility.UrlEncode(name[0], Encoding.UTF8) + "/" + HttpUtility.UrlEncode(name[1], Encoding.UTF8) + id;
-                        
-                        writer.WriteLine("爬虫抓取任务完成！开始步骤三！");
-                        writer.WriteLine("耗时：" + e.Milliseconds + "毫秒");
-                        writer.WriteLine("线程：" + e.ThreadId + "");
-                        writer.WriteLine("地址：" + e.Uri.ToString() + "");
-                        writer.WriteLine("结果：" + links + name[0] + name[1] + "");
-                        writer.WriteLine("===============================================");
                     }
                 }
-                GetPictures(url, e.Uri.ToString() + "");
+                GetPictures(pictrueUrlManaeger, e.Uri.ToString() + "", pictrueFilePath);
+                Console.WriteLine("目录：" + pictrueFilePath + "完成");
             };
 
-
-            var nameToUrl = manager.GetUrl();
-            var url1 = firstReferer + nameToUrl.Url;
-            var specialArguments = new Dictionary<string, string>
-            {
-                { "Name" , $"{nameToUrl.Name}"},
-            };
-            crawler.Start(new Uri(url1), refererUri, specialArguments);
-            /*Parallel.For(0, manager.Count, i =>
+            Parallel.For(0, manager.Count, i =>
             {
                 var nameToUrl = manager.GetUrl();
                 var url = firstReferer + nameToUrl.Url;
@@ -170,12 +191,12 @@ namespace WebCrawler.Bussiness
                 {
                     { "Name" , $"{nameToUrl.Name}"},
                 };
-                crawler.Start(new Uri(url), refererUri, specialArguments);
-            });*/
+                crawler.Start(new Uri(url), refererUri, specialArguments).Wait();
+            });
 
         }
 
-        public void GetPictures(string uri, string refererUri)
+        public void GetPictures(NameToUrlManage pictrueUrlManager, string refererUri, string pictrueFilePath)
         {
             var crawler = new MyCrawler();
 
@@ -214,10 +235,10 @@ namespace WebCrawler.Bussiness
                     using (var writer = new StreamWriter(stream))
                     {
                         var uriStr = e.Uri.ToString().Split('/');
-                        var filePathPicture = filePath + "\\" + uriStr[uriStr.Length - 1];
+                        var pictrueFilePathCurr = pictrueFilePath + "\\" + uriStr[uriStr.Length - 1];
                         var memoryStream = new MemoryStream(e.Bitmap);
                         var bitmap = new Bitmap(memoryStream);
-                        bitmap.Save(filePathPicture, ImageFormat.Jpeg);
+                        bitmap.Save(pictrueFilePathCurr, ImageFormat.Jpeg);
                         writer.WriteLine("爬虫抓取任务结束！");
                         writer.WriteLine("耗时：" + e.Milliseconds + "毫秒");
                         writer.WriteLine("线程：" + e.ThreadId + "");
@@ -227,17 +248,11 @@ namespace WebCrawler.Bussiness
                 }
             };
 
-            for (int i = 1;; i++)
+            for (int i = 0; i < manager.Count; i++)
             {
-                var uriTarget = uri + "/" + IntParseString(i) + ".jpg";
-                crawler.Start(new Uri(uriTarget), refererUri).Wait();
+                crawler.Start(new Uri(pictrueUrlManager.GetUrl().Url), refererUri).Wait();
             }
         }
 
-        public string IntParseString(int num)
-        {
-            var numStr = num.ToString();
-            return numStr;
-        }
     }
 }
