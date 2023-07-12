5fa744371cc0483b016719949b55336eead74da6
alexcxd <44088437+alexcxd@users.noreply.github.com>
2023-07-12 23:41:50 +0800

    建造者模式优化

--- SampleCode/SampleCode/DesignPattern/CreationalPatterns/BuilderPattern.cs
+++ SampleCode/SampleCode/DesignPattern/CreationalPatterns/BuilderPattern.cs
@@ -9,107 +9,110 @@ namespace SampleCode.DesignPattern.CreationalPatterns
     /// </summary>
     public class BuilderPattern
     {
-        public static void BuliderPatternMain()
+        public static void BuilderPatternMain()
         {
+            var concreteBuilderA = new ConcreteBuilderA();
+            var concreteBuilderB = new ConcreteBuilderB();
 
-            ConcreteBuilderA concreteBuilderA = new ConcreteBuilderA();
-            ConcreteBuilderB concreteBuilderB = new ConcreteBuilderB();
+            var director = new Director();
+            var aProduct = director.GetProduct(concreteBuilderA);
+            aProduct.Show();
 
-            Director director = new Director();
-            director.Construct(concreteBuilderA);
-            var aProcudt = concreteBuilderA.GetResult();
-            aProcudt.Show();
-
-            director.Construct(concreteBuilderB);
-            var bProduct = concreteBuilderB.GetResult();
+            var bProduct = director.GetProduct(concreteBuilderB);
             bProduct.Show();
 
         }
-    }
 
-    public class Product
-    {
-        private List<string> parts = new List<string>();
-        public void Add(string part)
+        /// <summary>
+        /// 产品类
+        /// </summary>
+        public class Product
         {
-            parts.Add(part);
-        }
-        public void Show()
-        {
-            Console.WriteLine("产品创建");
-            foreach(var part in parts)
+            private List<string> parts = new List<string>();
+            public void Add(string part)
             {
-                Console.WriteLine(part);
+                parts.Add(part);
+            }
+            public void Show()
+            {
+                Console.WriteLine("产品创建");
+                foreach (var part in parts)
+                {
+                    Console.WriteLine(part);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 建造者抽象类
+        /// </summary>
+        public abstract class Builder
+        {
+            public abstract void BuilderPartA();
+            public abstract void BuilderPartB();
+            public abstract Product GetResult();
+        }
+
+        /// <summary>
+        /// 具体建造类
+        /// </summary>
+        public class ConcreteBuilderA : Builder
+        {
+            private Product product = new Product();
+
+            public override void BuilderPartA()
+            {
+                product.Add("部件A");
+            }
+
+            public override void BuilderPartB()
+            {
+                product.Add("部件B");
+            }
+
+            public override Product GetResult()
+            {
+                return product;
+            }
+        }
+
+        /// <summary>
+        /// 具体建造类
+        /// </summary>
+        public class ConcreteBuilderB : Builder
+        {
+            private Product product = new Product();
+
+            public override void BuilderPartA()
+            {
+                product.Add("部件X");
+            }
+
+            public override void BuilderPartB()
+            {
+                product.Add("部件Y");
+            }
+
+            public override Product GetResult()
+            {
+                return product;
+            }
+        }
+
+        /// <summary>
+        /// 指挥者类
+        /// </summary>
+        /// <remarks>用来指挥建造过程</remarks>
+        public class Director
+        {
+            public Product GetProduct(Builder builder)
+            {
+                builder.BuilderPartA();
+                builder.BuilderPartB();
+
+                return builder.GetResult();
             }
         }
     }
 
-    /// <summary>
-    /// 建造者抽象类
-    /// </summary>
-    public abstract class Builder
-    {
-        public abstract void BuilderPartA();
-        public abstract void BuilderPartB();
-        public abstract Product GetResult();
-    }
-
-    /// <summary>
-    /// 具体建造类
-    /// </summary>
-    public class ConcreteBuilderA : Builder
-    {
-        private Product product = new Product();
-
-        public override void BuilderPartA()
-        {
-            product.Add("部件A");
-        }
-
-        public override void BuilderPartB()
-        {
-            product.Add("部件B");
-        }
-
-        public override Product GetResult()
-        {
-            return product;
-        }
-    }
-
-    /// <summary>
-    /// 具体建造类
-    /// </summary>
-    public class ConcreteBuilderB : Builder
-    {
-        private Product product = new Product();
-
-        public override void BuilderPartA()
-        {
-            product.Add("部件X");
-        }
-
-        public override void BuilderPartB()
-        {
-            product.Add("部件Y");
-        }
-
-        public override Product GetResult()
-        {
-            return product;
-        }
-    }
-
-    /// <summary>
-    /// 指挥者类
-    /// </summary>
-    /// <remarks>用来指挥建造过程</remarks>
-    public class Director
-    {
-        public void Construct(Builder builder)
-        {
-            builder.BuilderPartA();
-            builder.BuilderPartB();
-        }
-    }
 }
