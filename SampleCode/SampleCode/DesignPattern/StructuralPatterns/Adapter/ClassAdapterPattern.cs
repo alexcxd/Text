12fa08ce518cb4659ad532dc2b3cc49b10e1787f
alexcxd <44088437+alexcxd@users.noreply.github.com>
2023-07-18 22:39:13 +0800

    适配器模式优化

---
+++ SampleCode/SampleCode/DesignPattern/StructuralPatterns/Adapter/ClassAdapterPattern.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace SampleCode.DesignPattern.StructuralPatterns
+{
+    /// <summary>
+    /// 适配器模式-类适配器
+    /// </summary>
+    /// 类适配器：这一实现使用了继承机制： 适配器同时继承两个对象的接口。 请注意， 这种方式仅能在支持多重继承的编程语言中实现。
+    public class ClassAdapterPattern
+    {
+        public static void ObjectAdapterPatternMain()
+        {
+            Target adapter = new Adapter();
+            adapter.Request();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public interface ITarget
+        {
+            void Request();
+        }
+
+        /// <summary>
+        /// 需要接口
+        /// </summary>
+        public class Target : ITarget
+        {
+            public void Request()
+            {
+                Console.WriteLine("普通请求");
+            }
+        }
+
+        /// <summary>
+        /// 现存接口
+        /// </summary>
+        public class Adaptee
+        {
+            public void SpecificRequest()
+            {
+                Console.WriteLine("特殊请求");
+            }
+        }
+
+        /// <summary>
+        /// 转换器
+        /// </summary>
+        public class Adapter : Adaptee, ITarget
+        {
+            public  void Request()
+            {
+                this.SpecificRequest();
+            }
+        }
+    }
+}
---
+++ SampleCode/SampleCode/DesignPattern/StructuralPatterns/Adapter/ObjectAdapterPattern.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace SampleCode.DesignPattern.StructuralPatterns
+{
+    /// <summary>
+    /// 适配器模式-对象适配器
+    /// </summary>
+    /// 将一个类转化为客户需要的另一个接口
+    /// 主要应用于希望应用一些现存类，但是接口又与复用环境不符合的情况
+    /// 对象适配器：适配器实现了其中一个对象的接口， 并对另一个对象进行封装。 所有流行的编程语言都可以实现适配器。
+    public class ObjectAdapterPattern
+    {
+        public static void ObjectAdapterPatternMain()
+        {
+            Target adapter = new Adapter();
+            adapter.Request();
+        }
+
+        //需要接口
+        public class Target
+        {
+            public virtual void Request()
+            {
+                Console.WriteLine("普通请求");
+            }
+        }
+
+        //现存接口
+        public class Adaptee
+        {
+            public void SpecificRequest()
+            {
+                Console.WriteLine("特殊请求");
+            }
+        }
+
+        //转换器
+        public class Adapter : Target
+        {
+            Adaptee target = new Adaptee();
+
+            public override void Request()
+            {
+                target.SpecificRequest();
+            }
+        }
+    }
+
+}
